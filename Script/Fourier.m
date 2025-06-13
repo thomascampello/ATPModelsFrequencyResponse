b7bc5d4ca8436e038883112e25e62f21772f5a2a
Thomas Campello <thomasmcampello@hotmail.com>
2025-06-13 01:21:57 -0300

    Update and rename Fourier.m to Script/Fourier.m

--- Script/Fourier.m
+++ Script/Fourier.m
@@ -1,27 +1,27 @@
 %   
-% Rotina para cálculo da transformada de Fourier. 
+% Rotina para cÃ¡lculo da transformada de Fourier. 
 %
 function [F,erro]=Fourier(Dt,n_Dt,t,ft,w,F1,I,tol)
 %
-% Definição das variáveis de entrada.
+% DefiniÃ§Ã£o das variÃ¡veis de entrada.
 %
-% Dt --> Passo de integração.
+% Dt --> Passo de integraÃ§Ã£o.
 %
-% n_Dt --> Número de intervalos de tempo.
+% n_Dt --> NÃºmero de intervalos de tempo.
 %
 % t --> Tempo.
 %
 % ft --> Resposta ao impulso.
 %
-% w --> Frequência (rad/s).
+% w --> FrequÃªncia (rad/s).
 %
 % F1 --> Transformada de Fourier sem truncamento do tempo nos pontos de
-%        frequência w(I).
+%        frequÃªncia w(I).
 %
-% I --> Índice dos pontos de frequência para cálculo da transformada de
+% I --> Ãndice dos pontos de frequÃªncia para cÃ¡lculo da transformada de
 %       Fourier sem trumcamento do tempo.
 %
-% Definição das variáveis auxiliares.
+% DefiniÃ§Ã£o das variÃ¡veis auxiliares.
 %
 a=exp(-1j.*w.*t(1));
 b=1./w;
@@ -33,43 +33,43 @@ g=d.*e;
 h=c.*(f+g);
 k=b.*f;
 %
-% Inicialização da transformada de Fourier (F) de ft.
+% InicializaÃ§Ã£o da transformada de Fourier (F) de ft.
 %
 F=zeros(size(a))+zeros(size(a))*1i;
 erro = 0;
 %
-% Loop para cálculo das contribuições DF_i de cada intervalo de tempo 
+% Loop para cÃ¡lculo das contribuiÃ§Ãµes DF_i de cada intervalo de tempo 
 % para a transformada F.
 %
 for i=1:n_Dt
     %
-    % Mostra a evolução do cálculo de F.
+    % Mostra a evoluÃ§Ã£o do cÃ¡lculo de F.
     %
     % if( mod(i,10001)==0 )
-    %     fprintf('Instante de tempo de integração: %e\n',t(i))
+    %     fprintf('Instante de tempo de integraÃ§Ã£o: %e\n',t(i))
     %     fprintf('\n')
     % end
     %
-    % Seleção dos valores da função nos limites do intevalo "i".
+    % SeleÃ§Ã£o dos valores da funÃ§Ã£o nos limites do intevalo "i".
     %
     ft_1=ft(i);
     ft_2=ft(i+1);
     %
-    % Cálculo da tangente de teta em cada intervalo.
+    % CÃ¡lculo da tangente de teta em cada intervalo.
     %
     tgt=(ft_2-ft_1)/Dt;
     %
-    % Cálculo da contribuição DF_i de cada intervalo para F.
+    % CÃ¡lculo da contribuiÃ§Ã£o DF_i de cada intervalo para F.
     %
     DF_i=a.*(tgt.*h+1j*ft_1*k);
     %
     a=a.*e;
     %
-    % Contribuição de DF_i para F.
+    % ContribuiÃ§Ã£o de DF_i para F.
     %
     F=F+DF_i;
     %
-    % Cálculo do erro de truncamento da integração no tempo.
+    % CÃ¡lculo do erro de truncamento da integraÃ§Ã£o no tempo.
     %
     if(nargin>5)
         erro=max(abs( ( F(I)-F1 )./F1 ) )*100;
